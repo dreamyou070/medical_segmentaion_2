d36914d6fa73098b2df414e57afdc2294ae3b65d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-12 00:20:32 +0900

    training

--- train.sh
+++ train.sh
@@ -2,26 +2,21 @@
 # brain -> BraTS2020_Segmentation_256
 # abdomen ->
 
-# 2_absolute_pe_segmentation_model_a_cross_focal_use_batch_norm_query
-# 4_absolute_pe_segmentation_model_c_cross_focal_use_batch_norm_query
-# 6_absolute_pe_segmentation_model_b_cross_focal_use_batch_norm_query
-
-port_number=51264
+port_number=51265
 category="medical"
-obj_name="teeth"
-trigger_word="teeth"
-benchmark="teeth_main"
+obj_name="brain"
+trigger_word="brain"
+benchmark="BraTS2020_Segmentation_256"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="1_segment_with_head_model"
-# --use_instance_norm
-# --binary_test
+file_name="1_new_data_absolute_pe_segmentation_model_a_use_dice_ce_loss_unet_crossattn_vae_train_training_weight_decay_20240411"
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_config \
- --main_process_port $port_number ../train.py --log_with wandb \
- --output_dir "../../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}" \
+
+accelerate launch --config_file ../../gpu_config/gpu_0_config \
+ --main_process_port $port_number train.py --log_with wandb \
+ --output_dir "../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 200 \
- --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
+ --pretrained_model_name_or_path ../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --train_data_path "/home/dreamyou070/MyData/anomaly_detection/medical/${obj_name}/${benchmark}/train" \
  --test_data_path "/home/dreamyou070/MyData/anomaly_detection/medical/${obj_name}/${benchmark}/test" \
  --network_dim 128 --network_alpha 64 \
