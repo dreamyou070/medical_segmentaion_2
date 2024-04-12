398bf3c52624a249b3739eb26ca82172872789d6
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-12 16:21:39 +0900

    Update train_cls_head_model.sh

--- train_cls_head_model.sh
+++ train_cls_head_model.sh
@@ -17,7 +17,7 @@ file_name="Finetune_segment_head"
 # --use_instance_norm
 # --binary_test
 
-accelerate launch --config_file ../../gpu_config/gpu_0_1_config \
+accelerate launch --config_file ../../gpu_config/gpu_0_1_2_3_config \
  --main_process_port $port_number train_cls_head_model.py --log_with wandb \
  --output_dir "../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 200 \
