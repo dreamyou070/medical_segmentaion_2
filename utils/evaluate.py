cdea6e910825c371cd074f4db9c292623a404605
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-12 18:51:45 +0900

    Update evaluate.py

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -42,9 +42,7 @@ def evaluation_check(segmentation_head, dataloader, device, text_encoder, unet, 
                 query = query_dict[layer][0].squeeze()  # head, pix_num, dim
                 res = int(query.shape[1] ** 0.5)
                 reshaped_query = reshape_batch_dim_to_heads_3D_4D(query)  # 1, res, res, dim
-                if res not in q_dict:
-                    q_dict[res] = []
-                q_dict[res].append(reshaped_query)
+                q_dict[res] = reshaped_query
             x16_out, x32_out, x64_out = q_dict[16], q_dict[32], q_dict[64]
             masks_pred = segmentation_head(x16_out, x32_out, x64_out)
             #######################################################################################################################
