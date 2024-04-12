b1ce15b6e23345b0f5e6c45932b2dfb143bbb39f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-12 22:39:03 +0900

    Update generate_image.sh

--- generate_image.sh
+++ generate_image.sh
@@ -10,12 +10,12 @@ file_name="Finetune_segment_head_with_featuregenerator"
 # --use_position_embedder \
 # 64 / 4
 # 128 / 64
-accelerate launch --config_file ../../../gpu_config/gpu_0_config \
+accelerate launch --config_file ../../gpu_config/gpu_0_config \
  --main_process_port $port_number generate_image.py \
  --pretrained_model_name_or_path ../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_dim 128 --network_alpha 64 \
+ --network_dim 64 --network_alpha 4 \
  --data_path "/home/dreamyou070/MyData/anomaly_detection/${category}/${obj_name}/${benchmark}/test" \
- --network_folder "../../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}/model" \
+ --network_folder "../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}/model" \
  --obj_name "${obj_name}" \
  --prompt "${trigger_word}" \
  --latent_res 64 \
