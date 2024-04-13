7dc6c307da83f5cd29a39317d956206046ef8f49
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-13 10:45:56 +0900

    seg state dict

--- generate_image.py
+++ generate_image.py
@@ -103,6 +103,10 @@ def main(args):
         state_dict = torch.load(pretrained_seg_dir)
         for k in state_dict.keys():
             print(f'original key = {k}')
+
+        seg_state_dict = segmentation_head.state_dict()
+        for k in seg_state_dict.keys():
+            print(f'segmentation key = {k}')
         segmentation_head.load_state_dict(state_dict)
         segmentation_head.to(accelerator.device, dtype=weight_dtype)
 
