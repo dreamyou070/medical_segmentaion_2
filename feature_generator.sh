e3cdb5ead3e561fa206938fae3baf565b7f0bdf8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-14 14:37:59 +0900

    feature generating

--- feature_generator.sh
+++ feature_generator.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-port_number=56666
+port_number=54152
 category="medical"
 obj_name="leader_polyp"
 trigger_word="leader_polyp"
@@ -9,7 +9,7 @@ layer_name='layer_3'
 sub_folder="up_16_32_64"
 file_name="1_Finetune_segment_head_merging_general_caption"
 
-accelerate launch --config_file ../../gpu_config/gpu_0_1_2_3_4_config \
+accelerate launch --config_file ../../gpu_config/gpu_0_1_2_3_config \
  --main_process_port $port_number feature_generator.py --log_with wandb \
  --output_dir "../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 200 \
