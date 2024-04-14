9022cdbad89b39f3c236af04b4fcaaa3e2967458
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-14 17:51:52 +0900

    feature generation

--- feature_generator.sh
+++ feature_generator.sh
@@ -1,14 +1,14 @@
 # !/bin/bash
 # language 가 분명 작용하는듯 하다.
 
-port_number=54177
+port_number=58888
 category="medical"
 obj_name="leader_polyp"
 trigger_word="leader_polyp"
-benchmark="bkai-igh-neopolyp"
+benchmark="Pranet"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="3_Finetune_segment_not_gen_per_image_caption"
+file_name="1_Finetune_segment_head_merging"
 
 accelerate launch --config_file ../../gpu_config/gpu_0_1_2_3_config \
  --main_process_port $port_number feature_generator.py --log_with wandb \
@@ -25,7 +25,7 @@ accelerate launch --config_file ../../gpu_config/gpu_0_1_2_3_config \
  --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
- --n_classes 3 \
+ --n_classes 2 \
  --mask_res 256 \
  --use_batchnorm \
  --use_dice_ce_loss \
@@ -33,4 +33,5 @@ accelerate launch --config_file ../../gpu_config/gpu_0_1_2_3_config \
  --init_latent_p 1.0 \
  --generator_loss_weight 1.0 \
  --segmentation_loss_weight 1.0 \
+ --generation \
  --use_image_by_caption
