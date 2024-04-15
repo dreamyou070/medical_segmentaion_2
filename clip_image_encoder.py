c3a148ffbadc128e9e8a885af06cf57c7273d113
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-15 15:06:04 +0900

    Update clip_image_encoder.py

--- clip_image_encoder.py
+++ clip_image_encoder.py
@@ -9,8 +9,6 @@ processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 image_path = 'data_sample/image/sample_200.jpg'
 image = Image.open(image_path)
 inputs = processor(images=image, return_tensors="pt", padding=True)#.data['pixel_values'] # [1,3,224,224]
-print(f'image_inputs = {inputs.shape}')
-
 model.to("cuda:0")
 #image_inputs = inputs.to("cuda:0")
 
