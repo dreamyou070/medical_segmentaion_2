001f461544f9a9d5a66ca0015390debd15b09a4d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-15 22:35:39 +0900

    modeling vit

--- model/modeling_vit.py
+++ model/modeling_vit.py
@@ -11,7 +11,7 @@ from transformers.modeling_utils import PreTrainedModel
 from transformers.pytorch_utils import find_pruneable_heads_and_indices, prune_linear_layer
 from transformers.utils import (add_code_sample_docstrings, add_start_docstrings,add_start_docstrings_to_model_forward,
                                 logging,replace_return_docstrings,)
-from .configuration_vit import ViTConfig
+from transformers.models.vit.configuration_vit import ViTConfig
 
 
 logger = logging.get_logger(__name__)
