25d91fc10ee831b86adbfc05117e5ec699e76df6
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-15 15:07:12 +0900

    Update clip_image_encoder.py

--- clip_image_encoder.py
+++ clip_image_encoder.py
@@ -10,7 +10,7 @@ image_path = 'data_sample/image/sample_200.jpg'
 image = Image.open(image_path)
 inputs = processor(images=image, return_tensors="pt", padding=True)#.data['pixel_values'] # [1,3,224,224]
 model.to("cuda:0")
-#image_inputs = inputs.to("cuda:0")
+image_inputs = inputs.to("cuda:0")
 
-image_features = model.get_image_features(**inputs)
-print(f'image_features = {image_features.shape}')
+#image_features = model.get_image_features(**inputs)
+#print(f'image_features = {image_features.shape}')
