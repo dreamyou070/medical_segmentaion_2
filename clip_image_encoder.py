ba048108ac116144ebed068635986409f9a98a79
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-15 15:49:10 +0900

    a

---
+++ clip_image_encoder.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import requests
+
+from transformers import CLIPProcessor, CLIPModel
+
+clip_image_model = CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
+
+image_path = 'data_sample/image/sample_200.jpg'
+image = Image.open(image_path)
+inputs = processor(images=image, return_tensors="pt", padding=True)#.data['pixel_values'] # [1,3,224,224]
+clip_image_model.to("cuda:0")
+image_inputs = inputs.to("cuda:0")
+
+image_features = clip_image_model.get_image_features(**inputs) # [batch, pix_num, dim = 784]
+print(f'image_features = {image_features.shape}')
--- feature_generator.py
+++ feature_generator.py
@@ -160,7 +160,9 @@ def main(args):
 
             if args.use_image_condition :
                 with torch.no_grad():
-                    encoder_hidden_states = clip_image_model(batch["image_condition"]) # [Batch, 1, 768]
+                    cond_input = batch["image_condition"]
+                    encoder_hidden_states = clip_image_model.get_image_features(**batch["image_condition"]) # [Batch, 1, 768]
+                    print(f'encoder_hidden_states = {encoder_hidden_states.shape}')
 
             if args.use_text_condition :
                 with torch.set_grad_enabled(True):
