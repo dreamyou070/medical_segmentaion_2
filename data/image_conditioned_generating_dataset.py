a9d06bf514c2ab36b469aa17d5d1f267bedc68ec
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-18 14:56:35 +0900

    image conditioned generating dataset

--- data/image_conditioned_generating_dataset.py
+++ data/image_conditioned_generating_dataset.py
@@ -99,7 +99,8 @@ class TrainDataset(Dataset):
 
         # [2] get condition image
         image = Image.open(self.image_paths[idx])
-        inputs = self.condition_img_tokenizer(images=image, return_tensors="pt").squeeze(0)
+        inputs = self.condition_img_tokenizer(images=image, return_tensors="pt")#.squeeze(0)
+        print(f'inputs = {inputs}')
 
         sample = {'image': mask_img,
                   'condition_image' : inputs}
