3cbedb3bd2d2cee7d07beb2e0602dda6cb220285
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-18 11:13:44 +0900

    a

--- data/dataset_pseudomap.py
+++ data/dataset_pseudomap.py
@@ -157,10 +157,10 @@ class TrainDataset_SegPseudoMap(Dataset):
                        self.n_classes))  # 3
         # 256,256,3
         gt[:, :, :class_num] = gt_arr_
-
+        pseudo_label = self.image_processor(Image.fromarray((gt * 255).astype(np.uint8)).convert('RGB'))
 
         gt = torch.tensor(gt).permute(2, 0, 1)  # 3,256,256
-        pseudo_label = self.image_processor(Image.fromarray((gt * 255).astype(np.uint8)).convert('RGB'))
+
 
         # [3] gt flatten
         gt_flat = gt_arr.flatten()  # 128*128
@@ -369,8 +369,9 @@ class TestDataset_SegPseudoMap(Dataset):
 
         # 256,256,3
         gt[:, :, :class_num] = gt_arr_
-        gt = torch.tensor(gt).permute(2, 0, 1)  # 3,256,256
         pseudo_label = self.image_processor(Image.fromarray((gt * 255).astype(np.uint8)).convert('RGB'))
+        gt = torch.tensor(gt).permute(2, 0, 1)  # 3,256,256
+
         # [3] gt flatten
         gt_flat = gt_arr.flatten()  # 128*128
 
