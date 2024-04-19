3d203b1ba28a8dd40087c91a51c21f64f8884519
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-19 18:28:51 +0900

    a

--- image_condition_ddpm.sh
+++ image_condition_ddpm.sh
@@ -12,7 +12,7 @@ file_name="16_ddpm" #
 # 3
 # except generation
 
-accelerate launch --config_file ../../gpu_config/gpu_0_1_2_3_4_config \
+accelerate launch --config_file ../../gpu_config/gpu_0_config \
  --main_process_port $port_number image_condition_ddpm.py --log_with wandb \
  --output_dir "../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 400 \
