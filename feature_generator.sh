0f10121a6f9c67c8c3c79b388cd1bc106af37368
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-20 13:37:15 +0900

    Update feature_generator.sh

--- feature_generator.sh
+++ feature_generator.sh
@@ -27,7 +27,7 @@ accelerate launch --config_file ../../gpu_config/gpu_0_1_2_3_4_5_config \
  --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
- --n_classes 3 \
+ --n_classes 2 \
  --mask_res 256 \
  --batch_size 1 \
  --use_batchnorm \
