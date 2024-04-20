df90fbaeb42e3f4f91dce319da3c3daf3e36c479
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-20 20:41:27 +0900

    loading

---
+++ ADE20K/adk20_dataset.py
@@ -0,0 +1,18 @@
+import os
+import IPython.display
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+import pickle as pkl
+from utils import utils_ade20k
+
+DATASET_PATH = '../dataset/'
+index_file = 'index_ade20k.pkl'
+with open(f'{DATASET_PATH}/{index_file}', 'rb') as f:
+    index_ade20k = pkl.load(f)
+
+print("File loaded, description of the attributes:")
+print('--------------------------------------------')
+for attribute_name, desc in index_ade20k['description'].items():
+    print('* {}: {}'.format(attribute_name, desc))
+print('--------------------------------------------\n')
