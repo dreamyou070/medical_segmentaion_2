376dc204da561a02186ee00e1a47fdb304f91fda
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-20 12:21:43 +0900

    Update blip_training.py

--- blip_training.py
+++ blip_training.py
@@ -101,7 +101,7 @@ def main(args):
 
             # [2] optimizing loss
             optimizer.zero_grad()
-            accelerator.backward(lm_loss.retains_graph(True))
+            accelerator.backward(lm_loss.retain_graph(True))
             optimizer.step()
             lr_scheduler.step()
 
