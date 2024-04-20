5c3ae92344f95896aa6fea8c0a41fa089161ab96
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-20 20:51:43 +0900

    uploading pkl data

--- ADE20K/adk20_dataset.py
+++ ADE20K/adk20_dataset.py
@@ -6,7 +6,7 @@ import numpy as np
 import pickle as pkl
 from utils import utils_ade20k
 
-DATASET_PATH = '../dataset/'
+DATASET_PATH = 'dataset/'
 index_file = 'index_ade20k.pkl'
 with open(f'{DATASET_PATH}/{index_file}', 'rb') as f:
     index_ade20k = pkl.load(f)
