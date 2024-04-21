1ab9e229f4236ddad3c583af3faa0b4b0bab36b2
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-21 12:50:16 +0900

    8th feature generator

--- feature_generator.sh
+++ feature_generator.sh
@@ -4,15 +4,15 @@
 # 5_segment_image_condition_light_decoder_not_use_cls_token
 # 6_segment_image_condition_light_decoder_only_use_cls_token
 # 7_segment_image_condition_light_decoder_reduction
-
-port_number=50006
+# 8_segment_image_condition_light_decoder_no_condition
+port_number=50008
 category="medical"
 obj_name="leader_polyp"
 trigger_word="leader_polyp"
 benchmark="Pranet"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="6_segment_image_condition_light_decoder_only_use_cls_token" #
+file_name="8_segment_image_condition_light_decoder_no_condition" #
 # 3 --not_use_cls_token --without_condition
 # except generation #\
 
@@ -44,4 +44,4 @@ accelerate launch --config_file ../../gpu_config/gpu_0_1_2_3_4_config \
  --image_processor 'vit' \
  --image_model_training \
  --light_decoder --use_noise_pred_loss \
- --only_use_cls_token
+ --without_condition
