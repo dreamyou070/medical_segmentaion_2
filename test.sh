02d0eb89aa3396bb36316d62dfbc1951b7a44fa8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-22 10:20:41 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,15 +10,14 @@ file_name="25_reducing_redundancy"
 # --use_position_embedder \
 # 64 / 4
 # 128 / 64
-accelerate launch --config_file ../../../gpu_config/gpu_0_config \
- --main_process_port $port_number ../test.py \
- --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
+accelerate launch --config_file ../../gpu_config/gpu_0_config \
+ --main_process_port $port_number test.py \
+ --pretrained_model_name_or_path ../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --network_dim 144 --network_alpha 4 \
  --network_weights "../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}/model/lora-000163.safetensors" \
  --segmentation_head_weights "../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}/segmentation/segmentation-000163.pt" \
  --output_dir "../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}/thesis_output" \
  --base_path "/home/dreamyou070/MyData/anomaly_detection/${category}/${obj_name}/${benchmark}/test" \
- --network_folder "../../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}/model" \
  --obj_name "${obj_name}" \
  --prompt "${trigger_word}" \
  --latent_res 64 \
