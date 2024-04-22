d649671d2b2d90a50c3e3071f85f2010225a241f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-23 00:11:54 +0900

    a

--- polyppvt/lib/pvtv2.py
+++ polyppvt/lib/pvtv2.py
@@ -5,8 +5,6 @@ from functools import partial
 
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 from timm.models.registry import register_model
-from timm.models.vision_transformer import _cfg
-from timm.models.registry import register_model
 
 import math
 
