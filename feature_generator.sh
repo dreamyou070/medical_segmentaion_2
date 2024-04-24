4da0e9863c45b474bf50e06740f4beaa2b31378f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-24 14:29:03 +0900

    feature generator

--- feature_generator.sh
+++ feature_generator.sh
@@ -1,29 +1,18 @@
 # !/bin/bash
 # language 가 분명 작용하는듯 하다.
 
-# 20_double_self_attention
-# 21_use_layer_norm_reducing_redundancy_use_weighted_reduct
-# 22_use_instance_norm_reducing_redundancy_use_weighted_reduct
-# 23_use_layer_norm_reducing_redundancy
-# 24_use_instance_norm_reducing_redundancy
-# 25_reducing_redundancy (I will do this!)
-# 26_reducing_redundancy_use_weighted_reduct
-# 27_extract_from_noise
-
-port_number=53132
+port_number=53333
 category="medical"
-obj_name="brain"
-trigger_word="brain"
-benchmark="BraTS2020_Segmentation_256"
+obj_name="leader_polyp"
+trigger_word="leader_polyp"
+benchmark="Pranet"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="31_pvt_image_encoder_with_position_embedder" #
-# 3 --not_use_cls_token --without_condition
-# except generation
+file_name="33_pvt_image_encoder_with_position_embedder_reverse" #
+# 3 --not_use_cls_token --without_condition except generation
 # --gt_ext_npy \  --use_position_embedder
 # 29_reducing_redundancy_without_noise_pred_with_position_embedding
-# --reverse \
-# --reducing_redundancy
+# --reverse --reducing_redundancy
 
 accelerate launch --config_file ../../gpu_config/gpu_0_1_2_3_4_config \
  --main_process_port $port_number feature_generator.py --log_with wandb \
@@ -40,9 +29,9 @@ accelerate launch --config_file ../../gpu_config/gpu_0_1_2_3_4_config \
  --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
- --n_classes 4 --mask_res 256 --batch_size 1 \
+ --n_classes 2 --mask_res 256 --batch_size 1 \
  --use_dice_ce_loss \
  --optimizer_args weight_decay=0.00005 \
  --use_image_condition \
  --image_processor 'pvt' \
- --image_model_training --gt_ext_npy
+ --image_model_training --reverse
