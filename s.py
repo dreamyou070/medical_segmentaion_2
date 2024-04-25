84744c8ffe068d0dbeac1a927eff6024144a44fb
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-25 14:41:40 +0900

    get overfitted features

--- s.py
+++
@@ -1,9 +0,0 @@
-import torch
-feat = torch.randn(160)
-memory_bank = [feat, feat, feat]
-memory_bank = torch.stack(memory_bank)
-print(memory_bank.size())
-
-sample = torch.randn(1,160,256,256)
-x = mean + std * sample
-print(x.size())
