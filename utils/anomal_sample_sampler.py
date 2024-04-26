470d052532f67f73a813ce1cfcd58a38b1742e55
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-26 17:10:10 +0900

    a

--- utils/anomal_sample_sampler.py
+++ utils/anomal_sample_sampler.py
@@ -28,9 +28,9 @@ class DiagonalGaussianDistribution(object):
             N = parameters.shape[0]
             self.parameters = self.parameters[N:,:]
 
-    def sample(self, mask_res):
+    def sample(self, mask_res, weight_dtype, device):
 
-        sample = torch.randn((mask_res * mask_res, 160))
+        sample = torch.randn((mask_res * mask_res, 160)).to(weight_dtype=weight_dtype, device=device)
         x = self.mean + self.std * sample
         return x
 
