c0aa8a56e17abd67e6dae91eb781a5737dcbb0bc
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-26 15:08:33 +0900

    Update test_genertator.sh

--- test_genertator.sh
+++ test_genertator.sh
@@ -10,7 +10,7 @@ layer_name='layer_3'
 sub_folder="up_16_32_64"
 file_name="35_pvt_image_encoder_with_position_embedder_mse_generator_loss" #
 save_folder="35_pvt_image_encoder_with_position_embedder_mse_generator_loss" #
-accelerate launch --config_file ../../gpu_config/gpu_config \
+accelerate launch --config_file ../../gpu_config/gpu_0_config \
  --main_process_port $port_number test_generator.py --log_with wandb \
  --output_dir "../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 100 \
