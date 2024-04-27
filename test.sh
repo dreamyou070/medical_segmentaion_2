fc990cc7ff316468f825a8903c49e4d8a32d83e6
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-27 20:54:41 +0900

    testing

--- test.sh
+++ test.sh
@@ -1,21 +1,19 @@
 # !/bin/bash
 
 port_number=50052
-category="medical"
-obj_name="leader_polyp"
-benchmark="Pranet"
+category="camouflaged"
+obj_name="CAMO-V.1.0-CVIU2019_sy"
+benchmark="CAMO-V.1.0-CVIU2019_sy"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="31_pvt_image_encoder_with_position_embedder"
-# --use_position_embedder \
-# 64 / 4
-# 128 / 64  --reducing_redundancy
+file_name="1_basis_vit" # best 0.852
+
 accelerate launch --config_file ../../gpu_config/gpu_0_config \
  --main_process_port $port_number test.py \
  --pretrained_model_name_or_path ../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --network_dim 144 --network_alpha 4 \
- --network_weights "../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}/model/lora-000143.safetensors" \
- --segmentation_head_weights "../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}/segmentation/segmentation-000143.pt" \
+ --network_weights "../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}/model/lora-000022.safetensors" \
+ --segmentation_head_weights "../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}/segmentation/segmentation-000022.pt" \
  --output_dir "../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}" \
  --base_path "/home/dreamyou070/MyData/anomaly_detection/${category}/${obj_name}/${benchmark}/test" \
  --obj_name "${obj_name}" \
@@ -25,4 +23,4 @@ accelerate launch --config_file ../../gpu_config/gpu_0_config \
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --n_classes 2 \
  --use_position_embedder \
- --mask_res 256 --use_image_condition --image_processor 'pvt'
+ --mask_res 256 --use_image_condition --image_processor 'vit'
