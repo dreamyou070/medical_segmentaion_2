a06ee06d199eb90f8fb931c8d55d0cd6210fb718
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-27 21:19:56 +0900

    Update feature_generation_online.sh

--- feature_generation_online.sh
+++ feature_generation_online.sh
@@ -1,14 +1,14 @@
 # !/bin/bash
 port_number=53999
 category="camouflaged"
-obj_name="CAMO-V.1.0-CVIU2019_sy"
+obj_name="CAMO"
 trigger_word="CAMO-V.1.0-CVIU2019_sy"
 benchmark="CAMO-V.1.0-CVIU2019_sy"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
 file_name="1_basis_vit" # best 0.852
 
-accelerate launch --config_file ../../gpu_config/gpu_0_1_config \
+accelerate launch --config_file ../../gpu_config/gpu_0_1_2_3_config \
  --main_process_port $port_number feature_generation_online.py --log_with wandb \
  --output_dir "../result/${category}/${obj_name}/${benchmark}/${sub_folder}/${file_name}" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 200 \
