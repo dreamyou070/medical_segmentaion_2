c6c17b864a4042a97c6c4ae73f359b41665309ed
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-05-08 01:55:11 +0900

    Update lora.py

--- model/lora.py
+++ model/lora.py
@@ -1074,11 +1074,11 @@ class LoRANetwork(torch.nn.Module):
                                     dim = self.conv_lora_dim
                                     alpha = self.conv_alpha
 
-                            if dim is None or dim == 0:
-                                if is_linear or is_linear_lora or is_conv2d_1x1 or (self.conv_lora_dim is not None or conv_block_dims is not None):
-                                    skipped.append(lora_name)
+                            #if dim is None or dim == 0:
+                            #    if is_linear or is_linear_lora or is_conv2d_1x1 or (self.conv_lora_dim is not None or conv_block_dims is not None):
+                            #        skipped.append(lora_name)
 
-                                continue
+                            #    continue
 
                             if block_wise == None :
                                 lora = module_class(lora_name,
