8d6e72654248e9fcbb09d8947d2917aab97e14a0
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-05-08 01:35:17 +0900

    Update lora.py

--- model/lora.py
+++ model/lora.py
@@ -1034,13 +1034,13 @@ class LoRANetwork(torch.nn.Module):
             skipped = []
             # prefix ...
             for name, module in root_module.named_modules():
-                print(f'unet module.__class__.__name__ = {module.__class__.__name__}')
 
                 if module.__class__.__name__ in target_replace_modules:
 
                     for child_name, child_module in module.named_modules():
 
                         is_linear = child_module.__class__.__name__ == "Linear"
+                        is_linear = child_module.__class__.__name__ == "LoRACompatibleLinear"
                         is_conv2d = child_module.__class__.__name__ == "Conv2d"
                         is_conv2d_1x1 = is_conv2d and child_module.kernel_size == (1, 1)
 
